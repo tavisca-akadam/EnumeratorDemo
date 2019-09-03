6c147b9c12ee0fa67eebf5dc8141b43c05218c6e
tavisca-akadam <ankadam@tavisca.com>
2019-09-03 12:19:06 +0530

    Using IEnumerable and IEnumerator Interface

---
+++ EnumeratorDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumeratorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] daysOfWeeks =
+            {
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wedensday",
+                "Thursday",
+                "Friday",
+                "Saturday"
+            };
+
+            EnumeratorPlayground.DisplayItems(daysOfWeeks);
+            Console.ReadKey(true);
+        }
+    }
+
+    public class EnumeratorPlayground
+    {
+        public static void DisplayItems<T>(IEnumerable<T> collection)
+        {
+            using (IEnumerator<T> enumerator = collection.GetEnumerator())
+            {
+                bool hasMoreItems = enumerator.MoveNext();
+                while(hasMoreItems)
+                {
+                    T item = enumerator.Current;
+                    Console.WriteLine(item);
+                    hasMoreItems = enumerator.MoveNext();
+                }
+            }
+        }
+    }
+}
